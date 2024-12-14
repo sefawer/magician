4342493e3befcf0f204d310f7b384885c30dd591
Sefa Alemdaroğulları <sefa@newky.co>
2024-12-15 02:00:08 +0300

    products column to text

--- database/migrations/2024_12_13_201032_orders.php
+++ database/migrations/2024_12_13_201032_orders.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
             $table->unsignedInteger('user_id');
-            $table->unsignedInteger('products');
+            $table->text('products');
             $table->float('total_cost');
             $table->unsignedInteger('active_campaign');
             $table->timestamps();
